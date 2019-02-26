6540b1e64b38611302c2c9fdd878b00496595a31
KnightShade <saurao2612@gmail.com>
2019-02-26 17:18:53 +0530

    initial commit

---
+++ DES/DESKey.py
@@ -0,0 +1,12 @@
+# description:
+# key is 64 bit
+# generate keys shud return a list of 16 keys
+
+# keyAsString = "MYDESKEY"
+# key = ""
+# for k in keyAsString:
+#     key += bin(ord(k))[2:].zfill(8)
+# print(len(key), key)
+
+# def generateKeys(key):
+#     return [k1, k2, k3, ..., k16]
