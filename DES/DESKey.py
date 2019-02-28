5f91f9a701105b01c4c7271d49a1dea6e1ffe5cb
KnightShade <saurao2612@gmail.com>
2019-02-28 21:52:03 +0530

    1% progress...

--- DES/DESKey.py
+++ DES/DESKey.py
@@ -1,12 +1,20 @@
+# references:
+# http://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm 
+# -> contains an example of whole DES process with all 16 keys
+# -> check if all keys u generate are same as in the site
+
+# https://academic.csuohio.edu/yuc/security/Chapter_06_Data_Encription_Standard.pdf
+# -> Its the DES reference pdf
+# -> contains the DES process with key generation
+
+# https://github.com/RobinDavid/pydes/blob/master/pydes.py
+# -> contains the DES program in python
+
+
 # description:
 # key is 64 bit
-# generate keys shud return a list of 16 keys
+# generateKeys shud return a list of 16 keys
 
-# keyAsString = "MYDESKEY"
-# key = ""
-# for k in keyAsString:
-#     key += bin(ord(k))[2:].zfill(8)
-# print(len(key), key)
-
+# key = "0001001100110100010101110111100110011011101111001101111111110001"
 # def generateKeys(key):
 #     return [k1, k2, k3, ..., k16]
