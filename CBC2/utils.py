f1f1a98f981ce1d558bb16274b49fb6d1cbf781e
Rajath Aithal <37878287+ayerajath@users.noreply.github.com>
2019-04-01 19:22:55 +0530

    Update utils.py

--- CBC2/utils.py
+++ CBC2/utils.py
@@ -20,3 +20,15 @@ def convertToAscii(cipher):
 # a method to perform xor with zfill number of bits in the result
 def xor(x, y, zfill):
     return bin(int(x, 2) ^ int(y, 2))[2:].zfill(zfill)
+
+# method to pad extra characters to match the required length of some multiple of 16
+def pad_chars(text):
+    textLength = len(text)
+    charsToPad = textLength % 16
+    afterPad = ""
+    if text[-1] != 'x' or text[-1] != 'X':
+        afterPad = text + ('x' * (16 - charsToPad))
+    else:
+        afterPad = text + ('z' * (16 - charsToPad))
+    binaryStr = convertToBinary(afterPad) + bin(16 - charsToPad)[2:].zfill(128)
+    return binaryStr
